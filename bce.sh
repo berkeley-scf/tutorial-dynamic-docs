238294c12e08f1d77657e3273e94fa27672e6764
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-08-05 15:15:50 -0700

    now have 2 external chunks in demo

--- bce.sh
+++ bce.sh
@@ -1,2 +1,3 @@
 Rscript -e "install.packages(c('knitr', 'rmarkdown'), repos = 'http://cran.cnr.berkeley.edu')"
+sudo apt-get update
 sudo apt-get install texinfo pandoc-citeproc lmodern highlight
--- demo.R
+++ demo.R
@@ -1,3 +1,7 @@
-## @knitr external_chunk
+## @knitr external_chunk_1
 a <- 7
 cat("a is ", a, ".\n", sep = "")
+
+## @knitr external_chunk_2
+a <- 9
+cat("Now, a is ", a, ".\n", sep = "")
