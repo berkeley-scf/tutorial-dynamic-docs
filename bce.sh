4b343bb7f769a557db21cf20cc0dfd6cbff6abf0
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2015-08-18 15:52:55 -0700

    updated bce.sh

--- bce.sh
+++ bce.sh
@@ -1,10 +1,9 @@
 # for using R Markdown 
-Rscript -e "install.packages(c('rmarkdown'), repos = 'http://cran.cnr.berkeley.edu')"
+Rscript -e "install.packages(c('rmarkdown'), repos = 'http://cran.cnr.berkeley.edu', lib = '~/R/x86_64-pc-linux-gnu-library/3.2')"
 sudo apt-get update
-sudo apt-get install -y texinfo pandoc pandoc-citeproc lmodern highlight
+sudo apt-get install -y pandoc pandoc-citeproc lmodern 
 
 # for using knitr+LaTeX
-sudo apt-get update
 sudo apt-get install -y texinfo highlight
 
 # for the bash kernel for Jupyter
