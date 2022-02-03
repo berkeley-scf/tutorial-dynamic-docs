3799cf5595a021130f5c4f6300cb80ac7d4fe9a1
Christopher Paciorek <paciorek@stat.berkeley.edu>
2022-02-02 16:15:16 -0800

    finish updating tutorial

--- demo.R
+++ demo.R
@@ -1,6 +1,10 @@
-## @knitr external_chunk_1
+## The first chunk uses one format for indicating a chunk of code,
+## and the second uses the knitr style.
+
+## ---- external_chunk_1 ----
 a <- 7
 cat("a is ", a, ".\n", sep = "")
+
 
 ## @knitr external_chunk_2
 a <- 9
